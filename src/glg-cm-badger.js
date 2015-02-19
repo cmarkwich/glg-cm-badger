6208670a81bbd97ecdacc9affb1db5e9d6a8ef04
David Hayes <drhayes@glgroup.com>
2015-02-19 13:52:24 -0600

    Ah, okay, have to use coffee to get browserify to work on polymer-serve.

--- src/glg-cm-badger.js
+++ src/glg-cm-badger.js
@@ -1,9 +1,16 @@
+var moment = require('moment');
+
 // Flag from epiquery/councilMember/profile/getFlags.mustache
 function normalizeFlag(flag) {
   var templateName = flag.FLAG_NAME.toLowerCase()
     .replace(/%?( ?- ?)|%| |\//gi, '-')
     .replace(/-$/gi, '');
   flag.templateName = templateName + '-flag';
+  var dates = ['ACTION_DATE', 'CREATE_DATE', 'DURATION', 'LAST_UPDATE_DATE', 'HISTORY_CREATE_DATE'];
+  dates.forEach(function(field) {
+    flag[field + '_RAW'] = flag[field];
+    flag[field] = moment(flag[field]);
+  });
   console.log(templateName, flag);
   return flag;
 }
